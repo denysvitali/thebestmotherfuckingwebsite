5088a22e18a031293fce3185e957a8a62e78b95f
Denys Vitali <denys@denv.it>
2016-05-26 15:47:20 +0200

    Some work done

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,23 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
+var htmlMinifier = require('gulp-html-minifier');
 var del = require('del');
 
 gulp.task('views', () => {
     return gulp.src('src/pug/*.pug')
         .pipe(pug({}))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/pug/*.pug')
+        .pipe(pug({}))
+        .pipe(htmlMinifier({
+            removeComments: true,
+            collapseWhitespace: true,
+            removeTagWhitespace: true
+        }))
         .pipe(gulp.dest('dist/'));
 });
 
@@ -24,8 +36,12 @@ gulp.task('css', () => {
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('clean', ()=>{
-  del('dist');
+gulp.task('clean', () => {
+    del('dist');
 });
 
-gulp.task('default', ['clean','views', 'css']);
+gulp.task('watch', () => {
+    return gulp.watch('src/**/*', ['views', 'less']);
+});
+
+gulp.task('default', ['clean', 'views', 'css']);
