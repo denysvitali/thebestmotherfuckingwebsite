e62eae5184afa9cb1856024b676810b83c287874
Denys Vitali <denys.vitali@coelis.ch>
2017-02-17 01:37:32 +0100

    Add Contrast + Inverted mode, closes #6

--- src/dist/js/main.fucking.js
+++ src/dist/js/main.fucking.js
@@ -1,4 +1,5 @@
 'use strict';
+// I wish I could have used ES6 extravaganza, but not everyone supports it :(
 var r = document.getElementById('rbw');
 var currentHue = 0;
 var hueAddition = 5;
@@ -16,4 +17,31 @@ function doThatFuckingColorThing()
   setTimeout(function(){doThatFuckingColorThing();}, 1000/25);
 }
 
+function someControl(id, textArr, className){
+  // You see? No fucking jQuery needed, check http://jsperf.com/getelementbyid-vs-jquery-id/44
+  var acbox = document.getElementById(id);
+  var clickEvent = ('ontouchstart' in window ? 'touchend' : 'click');
+  var el = document.getElementsByTagName('html')[0];
+  acbox.addEventListener(clickEvent, function(){
+    if(acbox.innerHTML === textArr[0]){
+      el.classList.add(className);
+      acbox.innerHTML = textArr[1];
+    }
+    else{
+      el.classList.remove(className);
+      acbox.innerHTML = textArr[0];
+    }
+  });
+}
+
+function addContrastControl(){
+  someControl('contrast', ['Add more contrast', 'Remove additional contrast'], 'contrast');
+}
+
+function addInvertedControl(){
+  someControl('invmode', ['Inverted mode', 'Normal mode'], 'inverted');
+}
+
 doThatFuckingColorThing();
+addContrastControl();
+addInvertedControl();
